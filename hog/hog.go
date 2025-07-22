309c2c04447a3620c4074c1f5b43b47d17ca198f
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-07-22 21:24:08 +0200

    More updates

--- hog/hog.go
+++ hog/hog.go
@@ -3,24 +3,56 @@ package hog
 import (
 	"fmt"
 	"image"
+	"image/color"
 )
 
 type HOG struct {
+	grayImg image.Image
 }
 
-func (h *HOG) CalculateGradient(img image.Image, template any) any {
-	ts := len(template.([]any))
+func NewHOG(grayImg image.Image) *HOG {
+	return &HOG{
+		grayImg: grayImg,
+	}
+}
 
-	bounds := img.Bounds()
+func (h *HOG) GetRegion(r, c, step int) [][]float32 {
+	result := make([][]float32, 3)
+
+	for i := range step {
+		result[i] = make([]float32, 3)
+
+		for j := range step {
+			value := float32(h.grayImg.At(r+i-1, c+j-1).(color.Gray).Y) / 257.0
+			result[i][j] = value
+		}
+	}
+
+	return result
+}
+
+func (h *HOG) CalculateGradient(template any) image.Image {
+	step := len(template.([]any))
+
+	bounds := h.grayImg.Bounds()
 
 	rect := image.Rectangle{
 		Min: image.Pt(0, 0),
-		Max: image.Pt(bounds.Max.X+ts-1, bounds.Max.Y+ts-1),
+		Max: image.Pt(bounds.Max.X+step-1, bounds.Max.Y+step-1),
 	}
 
 	newImg := image.NewRGBA(rect)
+	result := image.NewRGBA(rect)
 
-	fmt.Println(ts, newImg.Rect)
+	_ = newImg
 
-	return nil
+	for r := range h.grayImg.Bounds().Max.X {
+		for c := range h.grayImg.Bounds().Max.Y {
+			currentRegion := h.GetRegion(r, c, step)
+
+			fmt.Printf("(%v, %v) - %v\n", r, c, currentRegion)
+		}
+	}
+
+	return result
 }
--- hog/hog_test.go
+++ hog/hog_test.go
@@ -6,9 +6,58 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/kachaje/hog/hog"
+	"image/color"
+	"image/draw"
 	_ "image/jpeg"
+
+	"github.com/kachaje/hog/hog"
 )
+
+func TestGetRegion(t *testing.T) {
+	width := 3
+	height := 3
+
+	grayImg := image.NewGray(image.Rect(0, 0, width, height))
+
+	i := 0
+	for y := range height {
+		for x := range width {
+			i++
+			value := uint8(i * 255)
+			grayImg.SetGray(x, y, color.Gray{value})
+		}
+	}
+
+	h := hog.NewHOG(grayImg)
+
+	result := h.GetRegion(1, 1, 3)
+
+	target := [][]float32{
+		{
+			0.9922179, 0.98054475, 0.9688716,
+		},
+		{
+			0.98832685, 0.9766537, 0.96498054,
+		},
+		{
+			0.9844358, 0.97276264, 0.9610895,
+		},
+	}
+
+	if result == nil {
+		t.Fatal("Test failed")
+	}
+
+	for i := range target {
+		for j := range target[i] {
+			if result[i][j] != target[i][j] {
+				t.Fatalf(`Test failed. 
+Expected: %#v; 
+Actual: %#v`, result, target)
+			}
+		}
+	}
+}
 
 func TestCalculateGradient(t *testing.T) {
 	reader, err := os.Open(filepath.Join("..", "data", "flower.jpg"))
@@ -22,11 +71,15 @@ func TestCalculateGradient(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	h := hog.HOG{}
+	grayImg := image.NewGray(img.Bounds())
+
+	draw.Draw(grayImg, grayImg.Bounds(), img, img.Bounds().Min, draw.Src)
+
+	h := hog.NewHOG(grayImg)
 
 	template := []any{-1, 0, 1}
 
-	result := h.CalculateGradient(img, template)
+	result := h.CalculateGradient(template)
 
 	_ = result
 }
