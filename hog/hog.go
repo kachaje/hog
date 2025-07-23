7765fe210678b968b2f29d120878029324175e47
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-07-23 09:23:25 +0200

    Added ResizeImg helper

--- hog/hog.go
+++ hog/hog.go
@@ -2,6 +2,7 @@ package hog
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 )
 
@@ -13,4 +14,33 @@ func (h *HOG) ImgToGray(img image.Image) *image.Gray {
 	draw.Draw(grayImg, grayImg.Bounds(), img, img.Bounds().Min, draw.Src)
 
 	return grayImg
+}
+
+func (h *HOG) ResizeImg(img image.Image, height int) image.Image {
+	if height < 50 {
+		return img
+	}
+	bounds := img.Bounds()
+	imgHeight := bounds.Dy()
+	if height >= imgHeight {
+		return img
+	}
+	imgWidth := bounds.Dx()
+	resizeFactor := float32(imgHeight) / float32(height)
+	ratio := float32(imgWidth) / float32(imgHeight)
+	width := int(float32(height) * ratio)
+	resizedImage := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	var imgX, imgY int
+	var imgColor color.Color
+	for x := range width {
+		for y := range height {
+			imgX = int(resizeFactor*float32(x) + 0.5)
+			imgY = int(resizeFactor*float32(y) + 0.5)
+			imgColor = img.At(imgX, imgY)
+			resizedImage.Set(x, y, imgColor)
+		}
+	}
+
+	return resizedImage
 }
--- hog/hog_test.go
+++ hog/hog_test.go
@@ -43,3 +43,37 @@ func TestImgToGray(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestResizeImg(t *testing.T) {
+	reader, err := os.Open(filepath.Join("..", "data", "flower.jpg"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer reader.Close()
+
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := hog.HOG{}
+
+	newImg := h.ResizeImg(img, 64)
+
+	filename := "outputResized.png"
+
+	outputFile, err := os.Create(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		outputFile.Close()
+
+		os.Remove(filename)
+	}()
+
+	err = jpeg.Encode(outputFile, newImg, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
